915f5ad54c195e0ac2573c4b783cef88933d9949
Tyron Rieckmann <ty@tyrum.cc>
2022-07-13 11:27:50 -0700

    Improved nvim-cmp menu keybinds

--- dot_config/nvim/lua/plug.lua
+++ dot_config/nvim/lua/plug.lua
@@ -128,10 +128,16 @@ local plugs = {
             local cmp = require("cmp")
             cmp.setup({
                 mapping = {
+                    ["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item()),
+                    ["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item()),
                     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
                     ["<C-f>"] = cmp.mapping.scroll_docs(4),
                     ["<C-e>"] = cmp.mapping.close(),
-                    ["<c-y>"] = cmp.mapping.confirm({
+                    ["<TAB>"] = cmp.mapping.confirm({
+                        behavior = cmp.ConfirmBehavior.Insert,
+                        select = true,
+                    }),
+                    ["<CR>"]  = cmp.mapping.confirm({
                         behavior = cmp.ConfirmBehavior.Insert,
                         select = true,
                     })
