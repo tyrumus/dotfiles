173a6f97ed9aa12f88266305bd8e51def6306329
Ty Rieckmann <trieckmann@moog.com>
2024-08-08 08:53:13 -0700

    feat: nvim bootstrap improvement

--- home/dot_config/nvim/lua/plug.lua
+++ home/dot_config/nvim/lua/plug.lua
@@ -1,7 +1,7 @@
 -- lazy.nvim stuff
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local out = vim.fn.system({
         "git",
         "clone",
         "--filter=blob:none",
@@ -9,6 +9,15 @@ if not vim.loop.fs_stat(lazypath) then
         "--branch=stable",
         lazypath,
     })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
