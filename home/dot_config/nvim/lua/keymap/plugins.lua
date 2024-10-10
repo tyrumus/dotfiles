464531df18f48ee5fc7559380ebb15475e1ba490
Tyron Rieckmann <trieckmann@moog.com>
2024-10-10 08:18:56 -0700

    feat(nvim): LSP start/stop keybind

--- home/dot_config/nvim/lua/keymap/plugins.lua
+++ home/dot_config/nvim/lua/keymap/plugins.lua
@@ -5,3 +5,7 @@ vim.keymap.set({"n", "t"}, "<F3>", "<C-\\><C-n><cmd>FloatermNext<CR>")
 
 -- Lazy.nvim Sync
 vim.keymap.set("n", "<leader>l", "<cmd>Lazy sync<CR>")
+
+-- LSP
+vim.keymap.set("n", "<leader>o", "<cmd>LspStart<CR>")
+vim.keymap.set("n", "<leader>p", "<cmd>LspStop<CR>")
