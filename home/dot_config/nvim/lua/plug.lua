62799d3f2304184f7664322c3658ff41eade438b
Tyron Rieckmann <trieckmann@moog.com>
2026-03-24 11:25:24 -0700

    feat(nvim): bootstrap tree-sitter-cli instead of package manager install

--- home/dot_config/nvim/lua/plug.lua
+++ home/dot_config/nvim/lua/plug.lua
@@ -1,28 +1,42 @@
--- bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- generic bootstrap function
+function bootstrap_dl(name, path, cmd)
+    if not (vim.uv or vim.loop).fs_stat(path) then
+        local out = vim.fn.system(cmd)
 
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local out = vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    })
-
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out, "WarningMsg" },
-            { "\nPress any key to exit..." },
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
+        if vim.v.shell_error ~= 0 then
+            vim.api.nvim_echo({
+                { "Failed to download " .. name .. ":\n", "ErrorMsg" },
+                { out, "WarningMsg" },
+                { "\nPress any key to exit..." },
+            }, true, {})
+            vim.fn.getchar()
+            os.exit(1)
+        end
     end
 end
 
+-- bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+bootstrap_dl("lazy.nvim", lazypath, {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+})
+
 vim.opt.rtp:prepend(lazypath)
+
+-- bootstrap tree-sitter-cli
+-- Arch Linux has left this package out of date for almost 4 months, and now it's breaking my config
+local tspath = vim.env.HOME .. "/.local/share/n/lib/node_modules/tree-sitter-cli"
+bootstrap_dl("tree-sitter-cli", tspath, {
+    "npm",
+    "install",
+    "-g",
+    "tree-sitter-cli"
+})
 
 -- Don't need lewis6991/impatient.nvim anymore
 vim.loader.enable()
