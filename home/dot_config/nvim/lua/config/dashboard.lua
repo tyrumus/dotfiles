8d4089d2ea00783882c0eededc811d0420fe1d3a
Ty Rieckmann <ty@tyrumus.dev>
2026-04-28 19:06:33 -0700

    fix(nvim): reset .local/share/nvim root folder
    makes reset more comprehensive like it should've been. fixes bootstrap
    issues i've had on nvim v0.12 upgrades on some machines.

--- home/dot_config/nvim/lua/config/dashboard.lua
+++ home/dot_config/nvim/lua/config/dashboard.lua
@@ -71,7 +71,7 @@ local buttons = {
         button("l", "󰳿  Update language servers", "<cmd>Mason<CR>"),
         button("p", "󰐳  Update parsers", "<cmd>TSManager<CR>"),
         button("s", "  Settings", ":cd $HOME/.config/nvim | lua require('fzf-lua').files()<CR>"),
-        button("R", "  Reset", "<cmd>!rm -rf ~/.local/share/nvim/lazy && rm -rf ~/.local/state/nvim && rm -f ~/.config/nvim/lazy-lock.json<CR>"),
+        button("R", "  Reset", "<cmd>!rm -rf ~/.local/share/nvim && rm -rf ~/.local/state/nvim && rm -f ~/.config/nvim/lazy-lock.json<CR>"),
         button("q", "󰗼  Quit", "<cmd>q!<CR>"),
     },
     opts = {
